6e556e1bd366cbd6a504b32e02fb030da1cb592b
lee <hfutlj@163.com>
2018-05-02 22:18:46 +0800

    add simpleFactory DesignPattern

---
+++ SimpleFactory/src/main/java/Caculator.java
@@ -0,0 +1,7 @@
+public class Caculator {
+
+    public double execute(String symbol , double a, double b) throws Exception {
+        Operate operate = OperateFactory.getOperate(symbol);
+        return operate.getResult(a, b);
+    }
+}
